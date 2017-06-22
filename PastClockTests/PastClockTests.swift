fdb1790f8b4edbd20e849d9787a6bf733d05a927
Taki <kentucky060309@gmail.com>
2017-06-22 15:20:37 +0900

    Initial Commit

---
+++ PastClockTests/PastClockTests.swift
@@ -0,0 +1,36 @@
+//
+//  PastClockTests.swift
+//  PastClockTests
+//
+//  Created by Kenta Taki on 2017/06/22.
+//  Copyright © 2017年 com.takiken. All rights reserved.
+//
+
+import XCTest
+@testable import PastClock
+
+class PastClockTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
