ec95346d63d4fc5f848ca21e4cd02136c0c10c90
Taki <kentucky060309@gmail.com>
2017-06-27 04:30:41 +0900

    円グラフ実装 アイコン追加

--- PastClock/ViewController.swift
+++ PastClock/ViewController.swift
@@ -7,17 +7,29 @@
 //
 
 import UIKit
+import ChameleonFramework
+
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var pieChartView: PieChartView!
+    @IBOutlet weak var pieChartView2: PieChartView2!
+    
     let beaconFileController = BeaconManager()
     var fileName = ""
+    
     var dataList:[String] = []
+    var unitList:[String] = []
+    
+    
+    var segments = [Segment]()
+    var segments2 = [Segment2]()
 
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        getCsvData()
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -25,7 +37,7 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    @IBAction func getCsvData(_ sender: Any) {
+    func getCsvData() {
     //csvの中身を取り出してListに格納する関数
     
         //BeaconManagerクラスで取得したパス
@@ -45,15 +57,199 @@ class ViewController: UIViewController {
             
             let csvDatafromFile = try String(contentsOfFile:csvPath, encoding:String.Encoding.utf8)
             
+            //csvファイルの中身を改行区切りで、dataLIstに格納
             dataList = csvDatafromFile.components(separatedBy: "\n")
         }catch {
             print(error)
         }
-        print(dataList)
+        //print(dataList)
+        
+        createPie()
+        //print(BeaconIdList)
+        
+        
         
     }
+    
+    //時刻とBeaconIDを１セットで分解して、角度と色を決定させる関数
+    func createPie() {
+        
+        let numberOfList = dataList.count
+        var unitString = ""
+        
+        for i in 0..<numberOfList{
+            
+            //午前中だけ
+            if i <= 720 {
+            
+                unitString = dataList[i]
+                unitList = unitString.components(separatedBy: ",")
+                //print(unitList)
+            
+                let i = Double(unitList[0])!
+                let Angle = i / 720 * 360
+            
+            
+                let value = Int(unitList[1])
+                var Color:UIColor = UIColor.white
+                var beaconId = 0
+            
+                if value != nil{
+                    beaconId = value!
+                }
+                
+                if beaconId == 1{
+                    Color = UIColor.flatRed
+                }
+                if beaconId == 2{
+                    Color = UIColor.flatOrange
+                }
+                if beaconId == 3{
+                    Color = UIColor.flatYellow
+                }
+                if beaconId == 4{
+                    Color = UIColor.flatSand
+                }
+                if beaconId == 5{
+                    Color = UIColor.flatNavyBlue
+                }
+                if beaconId == 6{
+                    Color = UIColor.flatMagenta
+                }
+                if beaconId == 7{
+                    Color = UIColor.flatWatermelon
+                }
+                if beaconId == 8{
+                    Color = UIColor.flatSkyBlue
+                }
+                if beaconId == 9{
+                    Color = UIColor.flatGreen
+                }
+                if beaconId == 10{
+                    Color = UIColor.flatMint
+                }
+                if beaconId == 11{
+                    Color = UIColor.flatForestGreen
+                }
+                if beaconId == 12{
+                    Color = UIColor.flatPurple
+                }
+                if beaconId == 13{
+                    Color = UIColor.flatBrown
+                }
+                if beaconId == 14{
+                    Color = UIColor.flatPlum
+                }
+                if beaconId == 15{
+                    Color = UIColor.flatTeal
+                }
+                if beaconId == 16{
+                    Color = UIColor.flatLime
+                }
+                if beaconId == 17{
+                    Color = UIColor.flatPink
+                }
+                if beaconId == 18{
+                    Color = UIColor.flatCoffee
+                }
+                if beaconId == 19{
+                    Color = UIColor.flatPowderBlue
+                }
+                if beaconId == 20{
+                    Color = UIColor.flatBlue
+                }
 
+            
+                //print(Color)
+                segments.append(Segment(color: Color, angle: CGFloat(Angle)))
+                
+            //午後から
+            }
+            else{
+                unitString = dataList[i]
+                unitList = unitString.components(separatedBy: ",")
+                //print(unitList)
+                
+                let i = Double(unitList[0])!
+                let Angle = (i - 720) / 720 * 360
+                
+                
+                let value = Int(unitList[1])
+                var Color:UIColor = UIColor.white
+                var beaconId = 0
+                
+                if value != nil{
+                    beaconId = value!
+                }
+                
+                if beaconId == 1{
+                    Color = UIColor.flatRed
+                }
+                if beaconId == 2{
+                    Color = UIColor.flatOrange
+                }
+                if beaconId == 3{
+                    Color = UIColor.flatYellow
+                }
+                if beaconId == 4{
+                    Color = UIColor.flatSand
+                }
+                if beaconId == 5{
+                    Color = UIColor.flatNavyBlue
+                }
+                if beaconId == 6{
+                    Color = UIColor.flatMagenta
+                }
+                if beaconId == 7{
+                    Color = UIColor.flatWatermelon
+                }
+                if beaconId == 8{
+                    Color = UIColor.flatSkyBlue
+                }
+                if beaconId == 9{
+                    Color = UIColor.flatGreen
+                }
+                if beaconId == 10{
+                    Color = UIColor.flatMint
+                }
+                if beaconId == 11{
+                    Color = UIColor.flatForestGreen
+                }
+                if beaconId == 12{
+                    Color = UIColor.flatPurple
+                }
+                if beaconId == 13{
+                    Color = UIColor.flatBrown
+                }
+                if beaconId == 14{
+                    Color = UIColor.flatPlum
+                }
+                if beaconId == 15{
+                    Color = UIColor.flatTeal
+                }
+                if beaconId == 16{
+                    Color = UIColor.flatLime
+                }
+                if beaconId == 17{
+                    Color = UIColor.flatPink
+                }
+                if beaconId == 18{
+                    Color = UIColor.flatCoffee
+                }
+                if beaconId == 19{
+                    Color = UIColor.flatPowderBlue
+                }
+                if beaconId == 20{
+                    Color = UIColor.flatBlue
+                }
 
+            segments2.append(Segment2(color: Color, angle: CGFloat(Angle)))
+            
+            }
 
+        }
+        
+        self.pieChartView.segments = segments
+        self.pieChartView2.segments2 = segments2
+    }
 }
-
---
+++ Podfile
@@ -0,0 +1,21 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '10.0'
+
+target 'PastClock' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for PastClock
+  pod 'ChameleonFramework/Swift', :git => 'https://github.com/ViccAlexander/Chameleon.git'
+
+  target 'PastClockTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'PastClockUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
